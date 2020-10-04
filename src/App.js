39177d1d7a37e7d307283825868d536c2abe019a
wwwwwmw <william.manwai@gmail.com>
2020-10-04 21:14:47 +0800

    scrollbar trick added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Counter } from './components/counter/Counter';
 import './App.css';
 import SideBar from './Sidebar'
 import Feed from './Feed'
+import Widgets from './Widgets'
 function App() {
   return (
     <div className="App">      
@@ -13,6 +14,7 @@ function App() {
       {/* Feed */}
       <Feed/>
       {/* Widget */}
+      <Widgets/>
     </div>
   );
 }
--- src/Feed.js
+++ src/Feed.js
@@ -1,16 +1,68 @@
 import React from 'react'
+import './Feed.css';
+
 
 function Feed() {
   return (
-    <div>
-      <h1>I am feed</h1>
-      {/* Header */}
+    
+      
+      
+      <div className="feed">
+        {/* Header */}
+        <div className="feed__header">
+          <h2>Home</h2>
+        </div>
+
       {/* TweetBox */}
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
+      <h3>DWOIJQ</h3>
       {/* Post */}
       {/* Post */}
       {/* Post */}
       {/* Post */}
-    </div>
+      </div>
+    
+    
   )
 }
 
