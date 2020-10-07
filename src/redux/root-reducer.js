571cf3c7fd4c27480f5c367afbdd956c75046143
wwwwwmw <william.manwai@gmail.com>
2020-10-08 05:00:15 +0800

    clean code..

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,10 +1,7 @@
 import { combineReducers } from 'redux';
-// import userReducer from './user/user.reducer';
-// import directoryReducer  from './directory/directory.reducer'
-// import cartReducer from './cart/cart.reducer';
+
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-// import shopReducer from './shop/shop.reducer';
 import userReducer from '../redux/user/user.reducer'
 
 const persistConfig = {
